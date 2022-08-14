0da0a8e1d13442f93572692563b0f5c306777fc3
Benjamin Sloutsky <ben.sloutsky@gmail.com>
2022-08-14 01:39:09 -0700

    First

---
+++ server/models/Users.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+    }
+})
+
+const UserModel = mongoose.model("users", UserSchema);
+module.exports = UserModel;
